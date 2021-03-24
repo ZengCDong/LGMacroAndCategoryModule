601410fb404a391148615ae8522340a4db55f42c
ZengCDong <983217036@qq.com>
2021-03-24 10:23:39 +0800

    first commit
    Signed-off-by: ZengCDong <983217036@qq.com>

---
+++ LGMacroAndCategoryModule/Classes/UIViewController+CurrentViewController.h
@@ -0,0 +1,17 @@
+//
+//  UIViewController+CurrentViewController.h
+//  AFNetworking
+//
+//  Created by fenwei on 2021/3/23.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIViewController (CurrentViewController)
++ (UIViewController *)currentViewController;
+
+@end
+
+NS_ASSUME_NONNULL_END
